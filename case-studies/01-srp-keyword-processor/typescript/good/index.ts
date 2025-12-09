04f6256c8832f8fd9574b0aa2e14242f88236cf7
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-12-09 16:47:14 +0600

    add index.ts

---
+++ case-studies/01-srp-keyword-processor/typescript/good/index.ts
@@ -0,0 +1,34 @@
+import { SearchVolumeService } from "./services/Api";
+import { CsvFileParser } from "./services/Parser";
+import { StatsRepository } from "./services/Repository";
+import { KeywordValidator } from "./services/Validator";
+
+class KeywordOrchestrator {
+   constructor(
+      private parser: CsvFileParser,
+      private validator: KeywordValidator,
+      private api: SearchVolumeService,
+      private repo: StatsRepository
+   ) {}
+
+   run(filePath: string) {
+      const keywords = this.parser.read(filePath);
+
+      for (const keyword of keywords) {
+         if (!this.validator.isValid(keyword)) continue;
+
+         const volume = this.api.getVolume(keyword);
+         this.repo.save({ term: keyword, volume });
+      }
+   }
+}
+
+// Usage
+const app = new KeywordOrchestrator(
+   new CsvFileParser(),
+   new KeywordValidator(),
+   new SearchVolumeService(),
+   new StatsRepository()
+);
+
+app.run("data.csv");
